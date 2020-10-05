c9fc03c0c7e4c14b7c3e0c9522aeda3da3a72e73
abcd567a <abcd567@hotmail.com>
2020-10-05 12:59:20 -0400

    Update install-piaware.sh

--- install-piaware.sh
+++ install-piaware.sh
@@ -5,7 +5,7 @@ INSTALL_DIRECTORY=${PWD}
 echo -e "\e[32mUpdating\e[39m"
 sudo apt update
 echo -e "\e[32mInstalling build tools\e[39m"
-sudo apt install -y git dh-systemd devscripts pkg-config
+sudo apt install -y git debhelper dh-systemd
 
 echo -e "\e[32mBuilding & Installing tcl-tls from source code. \e[39m"
 echo -e "\e[32mInstalling tcl-tls dependencies \e[39m"
@@ -25,7 +25,7 @@ sudo dpkg -i tcl-tls_*.deb
 
 
 echo -e "\e[32mInstalling piaware dependencies \e[39m"
-sudo apt install -y tcl8.6-dev python3-dev libz-dev 
+sudo apt install -y python3-dev python3-venv 
 sudo apt install -y libboost-system-dev libboost-program-options-dev
 sudo apt install -y libboost-regex-dev libboost-filesystem-dev
 sudo apt install -y net-tools tclx8.4 tcllib itcl3
@@ -33,23 +33,18 @@ sudo apt install -y net-tools tclx8.4 tcllib itcl3
 
 echo -e "\e[32mCloning piaware source code and building package \e[39m"
 cd ${INSTALL_DIRECTORY}
-git clone http://github.com/flightaware/piaware_builder     
+git clone http://github.com/flightaware/piaware_builder
+
+echo -e "\e[32mBuilding the piaware package \e[39m"
 cd ${INSTALL_DIRECTORY}/piaware_builder
-sudo ./sensible-build.sh bionic
-
-echo -e "\e[32mCarrying out Workaround for pyvenv  \e[39m"
-cd ${INSTALL_DIRECTORY}/piaware_builder/package-bionic
-sudo sed -i 's/pyvenv $(VENV) --without-pip/$(PYTHON3) -m venv $(VENV) --without-pip/' debian/rules
-sudo sed -i 's/python3-venv, //' debian/control
-
-echo -e "\e[32mbuilding the piaware package \e[39m"
+sudo ./sensible-build.sh buster
+cd ${INSTALL_DIRECTORY}/piaware_builder/package-buster
 sudo dpkg-buildpackage -b --no-sign 
 
 echo -e "\e[32mInstalling piaware package\e[39m"
 cd ../
 sudo dpkg -i piaware_*.deb
 
-echo -e "\e[32mEnabling piaware (for Kali Linux)\e[39m"
 sudo systemctl enable piaware
 sudo systemctl restart piaware
 
