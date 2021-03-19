37d71a4a5a74eb80046c69141db4c4af7ced6d09
abcd567a <abcd567@hotmail.com>
2021-03-19 07:06:53 -0400

    Update install-dump978-fa.sh

--- install-dump978-fa.sh
+++ install-dump978-fa.sh
@@ -17,17 +17,17 @@ git clone https://github.com/flightaware/dump978
 cd ${INSTALL_DIRECTORY}/dump978
 git fetch --all
 git reset --hard origin/master
+VER=$(git describe --tags | sed 's/^v//')
 echo -e "\e[32mBuilding dump978-fa package\e[39m"
 sudo dpkg-buildpackage -b --no-sign
 
-
 echo -e "\e[32mInstalling dump978-fa and SkyAware978 \e[39m"
 cd ../
-sudo dpkg -i dump978-fa_*.deb 
+sudo dpkg -i dump978-fa_${VER}_*.deb 
 sudo systemctl enable dump978-fa
 sudo systemctl restart dump978-fa
 
-sudo dpkg -i skyaware978_*.deb 
+sudo dpkg -i skyaware978_${VER}_*.deb 
 sudo systemctl enable skyaware978
 sudo systemctl restart skyaware978
 
