359cedd1ef9ec57332e9f5b4c37f89360df5063e
abcd567a <abcd567@hotmail.com>
2021-03-23 03:26:22 -0400

    Update install-dump1090-fa.sh

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 INSTALL_DIRECTORY=${PWD}
 
-echo -e "\e[32mUpdating\e[39m"
+echo -e "\e[32mFor Kali 2021, adding debian Buster repository to get all packages needed\e[39m"
+sudo echo "deb http://deb.debian.org/debian/ buster main" >> /tmp/deb.list 
+sudo mv /tmp/deb.list /etc/apt/sources.list.d/
 sudo apt update
 echo -e "\e[32mInstalling build tools\e[39m"
 sudo apt install -y git build-essential debhelper pkg-config dh-systemd
