95d00f5b64afde1bfa7c599eb13c504407949e69
abcd567a <abcd567@hotmail.com>
2022-01-30 05:38:26 -0500

    Update install-piaware-web.sh

--- install-piaware-web.sh
+++ install-piaware-web.sh
@@ -6,22 +6,24 @@ echo -e "\e[32mUpdating\e[39m"
 sudo apt update
 echo -e "\e[32mInstalling build tools\e[39m"
 sudo apt install -y git
+sudo apt install -y build-essential
 sudo apt install -y debhelper
-sudo apt install -y dh-systemd
+
 
 echo -e "\e[32mCloning piaware-web source code \e[39m"
 cd  ${INSTALL_DIRECTORY}
-git clone https://github.com/flightaware/piaware-web 
+git clone https://github.com/flightaware/piaware-web
 cd  ${INSTALL_DIRECTORY}/piaware-web
 git fetch --all
 git reset --hard origin/master
-#VER=$(git describe --tags | sed 's/^v//')
+
 echo -e "\e[32mbuilding piaware-web package \e[39m"
-#if [[ `lsb_release -sc` == "kali-rolling" ]]; then
-sudo sed -i 's/dh-systemd,//' debian/control
-#fi
+sudo ./prepare-build.sh bullseye
+cd  ${INSTALL_DIRECTORY}/piaware-web/package-bullseye
+
 sudo dpkg-buildpackage -b --no-sign
 VER=$(grep "Version:" debian/piaware-web/DEBIAN/control | sed 's/^Version: //')
+
 echo -e "\e[32mInstalling piaware-web package \e[39m"
 cd ../
 sudo dpkg -i piaware-web_${VER}_all.deb
