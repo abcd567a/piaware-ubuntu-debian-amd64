70418746b71c3fbf97d600507a9eee2a7dc56840
abcd567a <abcd567@hotmail.com>
2022-01-30 02:00:48 -0500

    Update install-piaware.sh

--- install-piaware.sh
+++ install-piaware.sh
@@ -4,45 +4,36 @@ INSTALL_DIRECTORY=${PWD}
 
 echo -e "\e[32mUpdating\e[39m"
 sudo apt update
-echo -e "\e[32mInstalling build tools\e[39m"
+echo -e "\e[32mInstalling Build tools & Build dependencies\e[39m"
 
+#Build-Tools
 sudo apt install -y git
 sudo apt install -y build-essential
+
+#Build-Depends: 
 sudo apt install -y debhelper
-sudo apt install -y pkg-config
-sudo apt install -y dh-systemd
-
-echo -e "\e[32mBuilding & Installing tcl-tls from source code. \e[39m"
-echo -e "\e[32mInstalling tcl-tls dependencies \e[39m"
-sudo apt install -y libssl-dev
-sudo apt install -y tcl-dev
-sudo apt install -y chrpath
-
-echo -e "\e[32mCloning tcl-tls source code \e[39m"
-
-cd  ${INSTALL_DIRECTORY}
-git clone https://github.com/flightaware/tcltls-rebuild 
-cd  ${INSTALL_DIRECTORY}/tcltls-rebuild
-git fetch --all
-git reset --hard origin/dev
-echo -e "\e[32mbuilding tcl-tls package \e[39m"
-./prepare-build.sh buster
-cd package-buster
-sudo dpkg-buildpackage -b --no-sign
-echo -e "\e[32mInstalling tcl-tls package \e[39m"
-cd ../
-sudo dpkg -i tcl-tls_*.deb
-
-echo -e "\e[32mInstalling piaware dependencies \e[39m"
+sudo apt install -y tcl8.6-dev
+sudo apt install -y autoconf
 sudo apt install -y python3-dev
-sudo apt install -y python3-venv 
+sudo apt install -y python3-venv
+sudo apt install -y python3-setuptools
+sudo apt install -y libz-dev
+sudo apt install -y openssl
 sudo apt install -y libboost-system-dev
 sudo apt install -y libboost-program-options-dev
 sudo apt install -y libboost-regex-dev
 sudo apt install -y libboost-filesystem-dev
+sudo apt install -y patchelf
+
+echo -e "\e[32mInstalling piaware dependencies \e[39m"
+
+#Depends:
 sudo apt install -y net-tools
+sudo apt install -y iproute2
 sudo apt install -y tclx8.4
+sudo apt install -y tcl8.6
 sudo apt install -y tcllib
+sudo apt install -y tcl-tls
 sudo apt install -y itcl3
 
 echo -e "\e[32mCloning piaware source code and building package \e[39m"
@@ -51,14 +42,9 @@ git clone https://github.com/flightaware/piaware_builder
 cd ${INSTALL_DIRECTORY}/piaware_builder
 git fetch --all
 git reset --hard origin/master
-#VER=$(git describe --tags | sed 's/^v//')
 echo -e "\e[32mBuilding the piaware package \e[39m"
-sudo ./sensible-build.sh buster
-cd ${INSTALL_DIRECTORY}/piaware_builder/package-buster
-
-#if [[ `lsb_release -sc` == "kali-rolling" ]]; then
-sudo sed -i 's/dh-systemd,//' debian/control
-#fi
+sudo ./sensible-build.sh bullseye
+cd ${INSTALL_DIRECTORY}/piaware_builder/package-bullseye
 
 sudo dpkg-buildpackage -b --no-sign 
 VER=$(grep "Version:" debian/piaware/DEBIAN/control | sed 's/^Version: //')
