3bfe3e57e02d25759399b0d4c9b60c62ee7ddcc2
abcd567a <abcd567@hotmail.com>
2022-09-11 19:37:39 -0400

    Update install-dump1090-fa.sh

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
+
 INSTALL_DIRECTORY=${PWD}
 
 echo -e "\e[32mUpdating\e[39m"
 sudo apt update
 
+## Detect OS Version
+OS_VERSION=`lsb_release -sc`
+echo -e "\e[35mDETECTED OS VERSION" ${OS_VERSION} "\e[39m"
+
+if [[ ${OS_VERSION} == bionic ]]; then
+  OS_VERSION=stretch
+  sudo apt install -y devscripts
+elif [[ ${OS_VERSION} == focal ]]; then
+  OS_VERSION=buster
+elif [[ ${OS_VERSION} == jammy ]]; then
+  OS_VERSION=bullseye
+elif [[ ${OS_VERSION} == bookworm ]]; then
+  OS_VERSION=bullseye
+fi
+
+echo -e "\e[36mBUILDING PACKAGE USING VER" ${OS_VERSION} "\e[39m"
+
 echo -e "\e[32mInstalling Build tools and Build dependencies\e[39m"
 
 ##Build-Tools
+if [[ ${OS_VERSION} == stretch ]]; then sudo apt install -y devscripts; fi
 sudo apt install -y git
 sudo apt install -y build-essential
 
@@ -37,15 +56,15 @@ git fetch --all
 git reset --hard origin/master
 
 echo -e "\e[32mBuilding dump1090-fa package\e[39m"
-sudo ./prepare-build.sh bullseye
-cd ${INSTALL_DIRECTORY}/dump1090/package-bullseye
+sudo ./prepare-build.sh ${OS_VERSION}
+cd ${INSTALL_DIRECTORY}/dump1090/package-${OS_VERSION}
 
 sudo dpkg-buildpackage -b --no-sign
-VER=$(grep "Version:" debian/dump1090-fa/DEBIAN/control | sed 's/^Version: //')
+DUMP_VER=$(grep "Version:" debian/dump1090-fa/DEBIAN/control | sed 's/^Version: //')
 
 echo -e "\e[32mInstalling dump1090-fa\e[39m"
 cd ../
-sudo dpkg -i dump1090-fa_${VER}_*.deb
+sudo dpkg -i dump1090-fa_${DUMP_VER}_*.deb
 
 sudo systemctl enable dump1090-fa
 sudo systemctl restart dump1090-fa
