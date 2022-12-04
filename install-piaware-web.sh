3132e4831db00d820a19662a41b4fd5966e8e48d
abcd567a <abcd567@hotmail.com>
2022-12-04 00:57:27 -0500

    Update install-piaware-web.sh

--- install-piaware-web.sh
+++ install-piaware-web.sh
@@ -4,11 +4,58 @@ INSTALL_DIRECTORY=${PWD}
 
 echo -e "\e[32mUpdating\e[39m"
 sudo apt update
+
+## Detect OS 
+OS_ID=`lsb_release -si`
+OS_RELEASE=`lsb_release -sr`
+OS_VERSION=`lsb_release -sc`
+
+echo -e "\e[35mDETECTED OS VERSION" ${OS_ID} ${OS_RELEASE} ${OS_VERSION}  "\e[39m"
+
+## DEBIAN
+if [[ ${OS_VERSION} == stretch ]]; then
+  OS_VERSION=stretch
+elif [[ ${OS_VERSION} == buster ]]; then
+  OS_VERSION=buster
+elif [[ ${OS_VERSION} == bullseye ]]; then
+  OS_VERSION=bullseye
+elif [[ ${OS_VERSION} == bookworm ]]; then
+  OS_VERSION=bullseye
+
+## UBUNTU
+elif [[ ${OS_VERSION} == bionic ]]; then
+  OS_VERSION=stretch
+elif [[ ${OS_VERSION} == focal ]]; then
+  OS_VERSION=buster
+elif [[ ${OS_VERSION} == jammy ]]; then
+  OS_VERSION=bullseye
+
+## LINUX MINT
+elif [[ ${OS_VERSION} == tara || ${OS_VERSION} == tessa || ${OS_VERSION} == tina || ${OS_VERSION} == tricia ]]; then
+  OS_VERSION=stretch
+elif [[ ${OS_VERSION} == una || ${OS_VERSION} == uma || ${OS_VERSION} == ulyana || ${OS_VERSION} == ulyssa ]]; then
+  OS_VERSION=buster
+elif [[ ${OS_VERSION} == vanessa || ${OS_VERSION} == vera ]]; then
+  OS_VERSION=bullseye
+
+## KALI LINUX
+elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2021 ]]; then
+  OS_VERSION=buster
+elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2022 ]]; then
+  OS_VERSION=bullseye
+
+else
+#  OS_VERSION=bullseye
+   echo -e "\e[01;31mdont know how to install on" ${OS_ID} ${OS_RELEASE} ${OS_VERSION} "\e[39m"
+   exit
+fi
+
+echo -e "\e[36mBUILDING PACKAGE USING DEBIAN VER" ${OS_VERSION} "\e[39m"
+
 echo -e "\e[32mInstalling build tools\e[39m"
 sudo apt install -y git
 sudo apt install -y build-essential
 sudo apt install -y debhelper
-
 
 echo -e "\e[32mCloning piaware-web source code \e[39m"
 cd  ${INSTALL_DIRECTORY}
@@ -18,8 +65,8 @@ git fetch --all
 git reset --hard origin/master
 
 echo -e "\e[32mbuilding piaware-web package \e[39m"
-sudo ./prepare-build.sh bullseye
-cd  ${INSTALL_DIRECTORY}/piaware-web/package-bullseye
+sudo ./prepare-build.sh ${OS_VERSION}
+cd  ${INSTALL_DIRECTORY}/piaware-web/package-${OS_VERSION}
 
 sudo dpkg-buildpackage -b --no-sign
 VER=$(grep "Version:" debian/piaware-web/DEBIAN/control | sed 's/^Version: //')
