1d875d4b6a01c1647eda002b1561d943c5fcf5b1
abcd567a <abcd567@hotmail.com>
2022-12-08 17:34:03 -0500

    Update install-dump978-fa.sh

--- install-dump978-fa.sh
+++ install-dump978-fa.sh
@@ -4,22 +4,54 @@ INSTALL_DIRECTORY=${PWD}
 echo -e "\e[32mUpdating\e[39m"
 sudo apt update
 
-## Detect OS Version
+## Detect OS 
+OS_ID=`lsb_release -si`
+OS_RELEASE=`lsb_release -sr`
 OS_VERSION=`lsb_release -sc`
-echo -e "\e[35mDETECTED OS VERSION" ${OS_VERSION} "\e[39m"
 
-if [[ ${OS_VERSION} == bionic ]]; then
+echo -e "\e[35mDETECTED OS VERSION" ${OS_ID} ${OS_RELEASE} ${OS_VERSION}  "\e[39m"
+
+## DEBIAN
+if [[ ${OS_VERSION} == stretch ]]; then
+  OS_VERSION=stretch
+elif [[ ${OS_VERSION} == buster ]]; then
+  OS_VERSION=buster
+elif [[ ${OS_VERSION} == bullseye ]]; then
+  OS_VERSION=bullseye
+elif [[ ${OS_VERSION} == bookworm ]]; then
+  OS_VERSION=bullseye
+
+## UBUNTU
+elif [[ ${OS_VERSION} == bionic ]]; then
   OS_VERSION=stretch
 elif [[ ${OS_VERSION} == focal ]]; then
   OS_VERSION=buster
 elif [[ ${OS_VERSION} == jammy ]]; then
   OS_VERSION=bullseye
-elif [[ ${OS_VERSION} == bookworm ]]; then
+elif [[ ${OS_VERSION} == kinetic ]]; then
   OS_VERSION=bullseye
+  
+## LINUX MINT
+elif [[ ${OS_VERSION} == tara || ${OS_VERSION} == tessa || ${OS_VERSION} == tina || ${OS_VERSION} == tricia ]]; then
+  OS_VERSION=stretch
+elif [[ ${OS_VERSION} == una || ${OS_VERSION} == uma || ${OS_VERSION} == ulyana || ${OS_VERSION} == ulyssa ]]; then
+  OS_VERSION=buster
+elif [[ ${OS_VERSION} == vanessa || ${OS_VERSION} == vera ]]; then
+  OS_VERSION=bullseye
+
+## KALI LINUX
+elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2021 ]]; then
+  OS_VERSION=buster
+elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2022 ]]; then
+  OS_VERSION=bullseye
+
+else
+#  OS_VERSION=bullseye
+   echo -e "\e[01;31mdont know how to install on" ${OS_ID} ${OS_RELEASE} ${OS_VERSION} "\e[39m"
+   exit
 fi
 
-echo -e "\e[36mBUILDING PACKAGE USING VER" ${OS_VERSION} "\e[39m"
-
+echo -e "\e[36mBUILDING PACKAGE USING DEBIAN VER" ${OS_VERSION} "\e[39m"
 
 echo -e "\e[32mInstalling Build tools and Build dependencies\e[39m"
 
