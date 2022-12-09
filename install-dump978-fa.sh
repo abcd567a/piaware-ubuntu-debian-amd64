a8c47c2c13933401d09b1441b0cfa7e5a99fab1b
abcd567a <abcd567@hotmail.com>
2022-12-08 19:40:59 -0500

    Update install-dump978-fa.sh

--- install-dump978-fa.sh
+++ install-dump978-fa.sh
@@ -85,7 +85,7 @@ git fetch --all
 git reset --hard origin/master
 
 echo -e "\e[32mBuilding dump978-fa package\e[39m"
-sudo mv package-${OS_VERSION} package-${OS_VERSION}-old
+sudo mv package-${OS_VERSION} package-${OS_VERSION}-old-$RANDOM
 sudo ./prepare-build.sh ${OS_VERSION}
 cd ${INSTALL_DIRECTORY}/dump978/package-${OS_VERSION}
 
