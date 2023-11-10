086838fdbf3bffbbf07541904897cbf2be85c665
abcd567a <abcd567@hotmail.com>
2023-11-10 07:47:26 -0500

    Update install-dump978-fa.sh

--- install-dump978-fa.sh
+++ install-dump978-fa.sh
@@ -23,7 +23,7 @@ elif [[ ${OS_VERSION} == buster ]]; then
 elif [[ ${OS_VERSION} == bullseye ]]; then
   OS_VERSION=bullseye
 elif [[ ${OS_VERSION} == bookworm ]]; then
-  OS_VERSION=bullseye
+  OS_VERSION=bookworm
 
 ## UBUNTU
 elif [[ ${OS_VERSION} == bionic ]]; then
@@ -35,7 +35,7 @@ elif [[ ${OS_VERSION} == jammy ]]; then
 elif [[ ${OS_VERSION} == kinetic ]]; then
   OS_VERSION=bullseye
 elif [[ ${OS_VERSION} == lunar ]]; then
-  OS_VERSION=bullseye
+  OS_VERSION=bookworm
   
 ## LINUX MINT
 elif [[ ${OS_VERSION} == tara || ${OS_VERSION} == tessa || ${OS_VERSION} == tina || ${OS_VERSION} == tricia ]]; then
@@ -51,10 +51,10 @@ elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2021 ]]; then
 elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2022 ]]; then
   OS_VERSION=bullseye
 elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2023 ]]; then
-  OS_VERSION=bullseye
+  OS_VERSION=bookworm
 
+## ANY OTHER
 else
-#  OS_VERSION=bullseye
    echo -e "\e[01;31mdont know how to install on" ${OS_ID} ${OS_RELEASE} ${OS_VERSION} "\e[39m"
    exit
 fi
