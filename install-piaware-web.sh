d85e32297d1ee27333fc30d7f2a16e6e0ae9f80e
abcd567a <abcd567@hotmail.com>
2023-11-10 07:52:22 -0500

    Update install-piaware-web.sh

--- install-piaware-web.sh
+++ install-piaware-web.sh
@@ -24,7 +24,7 @@ elif [[ ${OS_VERSION} == buster ]]; then
 elif [[ ${OS_VERSION} == bullseye ]]; then
   OS_VERSION=bullseye
 elif [[ ${OS_VERSION} == bookworm ]]; then
-  OS_VERSION=bullseye
+  OS_VERSION=bookworm
 
 ## UBUNTU
 elif [[ ${OS_VERSION} == bionic ]]; then
@@ -36,7 +36,7 @@ elif [[ ${OS_VERSION} == jammy ]]; then
 elif [[ ${OS_VERSION} == kinetic ]]; then
   OS_VERSION=bullseye
 elif [[ ${OS_VERSION} == lunar ]]; then
-  OS_VERSION=bullseye
+  OS_VERSION=bookworm
 
 ## LINUX MINT
 elif [[ ${OS_VERSION} == tara || ${OS_VERSION} == tessa || ${OS_VERSION} == tina || ${OS_VERSION} == tricia ]]; then
@@ -52,10 +52,10 @@ elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2021 ]]; then
 elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2022 ]]; then
   OS_VERSION=bullseye
 elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2023 ]]; then
-  OS_VERSION=bullseye
+  OS_VERSION=bookworm
 
+## ANY OTHER
 else
-#  OS_VERSION=bullseye
    echo -e "\e[01;31mdont know how to install on" ${OS_ID} ${OS_RELEASE} ${OS_VERSION} "\e[39m"
    exit
 fi
