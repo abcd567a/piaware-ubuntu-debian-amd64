663b0c0f41e1e6ad3664536f0c0a2c8cddab2e5c
abcd567a <abcd567@hotmail.com>
2023-11-10 08:42:07 -0500

    Update install-piaware.sh

--- install-piaware.sh
+++ install-piaware.sh
@@ -5,8 +5,8 @@ trap 'echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
 INSTALL_DIRECTORY=${PWD}
 
 echo -e "\e[32mUpdating\e[39m"
-sudo apt update
-sudo apt install -y lsb-release
+apt update
+apt install -y lsb-release
 
 ## Detect OS 
 OS_ID=`lsb_release -si`
@@ -62,37 +62,37 @@ echo -e "\e[36mBUILDING PACKAGE USING DEBIAN VER" ${OS_VERSION} "\e[39m"
 echo -e "\e[32mInstalling Build tools & Build dependencies\e[39m"
 
 #Build-Tools
-sudo apt install -y git
-sudo apt install -y build-essential
-sudo apt install -y devscripts
+apt install -y git
+apt install -y build-essential
+apt install -y devscripts
   
 #Build-Depends: 
-sudo apt install -y debhelper
-sudo apt install -y tcl8.6-dev
-sudo apt install -y autoconf
-sudo apt install -y python3-dev
-sudo apt install -y python3-venv
-sudo apt install -y python3-setuptools
-sudo apt install -y libz-dev
-sudo apt install -y openssl
-sudo apt install -y libboost-system-dev
-sudo apt install -y libboost-program-options-dev
-sudo apt install -y libboost-regex-dev
-sudo apt install -y libboost-filesystem-dev
-sudo apt install -y patchelf
+apt install -y debhelper
+apt install -y tcl8.6-dev
+apt install -y autoconf
+apt install -y python3-dev
+apt install -y python3-venv
+apt install -y python3-setuptools
+apt install -y libz-dev
+apt install -y openssl
+apt install -y libboost-system-dev
+apt install -y libboost-program-options-dev
+apt install -y libboost-regex-dev
+apt install -y libboost-filesystem-dev
+apt install -y patchelf
 
 echo -e "\e[32mBuilding & Installing tcl-tls from source code. \e[39m"
 echo -e "\e[32mInstalling tcl-tls dependencies \e[39m"
-sudo apt install -y libssl-dev
-sudo apt install -y tcl-dev
-sudo apt install -y chrpath
+apt install -y libssl-dev
+apt install -y tcl-dev
+apt install -y chrpath
 
 cd  ${INSTALL_DIRECTORY}
 
 if [[ -d tcltls-rebuild ]];
 then
 echo -e "\e[32mRenaming existing tcltls-rebuild folder by adding prefix \"old\" \e[39m"
-sudo mv tcltls-rebuild tcltls-rebuild-old-$RANDOM
+mv tcltls-rebuild tcltls-rebuild-old-$RANDOM
 fi
 
 echo -e "\e[32mCloning tcl-tls source code \e[39m"
@@ -101,29 +101,32 @@ cd  ${INSTALL_DIRECTORY}/tcltls-rebuild
 git fetch --all
 git reset --hard origin/master
 echo -e "\e[32mbuilding tcl-tls package \e[39m"
+if [[ ${OS_VERSION} == bookworm ]]; then OS_VERSION=bullseye; fi
 ./prepare-build.sh ${OS_VERSION}
 cd package-${OS_VERSION}
-sudo dpkg-buildpackage -b --no-sign
+dpkg-buildpackage -b --no-sign
 echo -e "\e[32mInstalling tcl-tls package \e[39m"
 cd ../
 sudo dpkg -i tcl-tls_*.deb
+OS_VERSION=`lsb_release -sc`
+echo -e "\e[36mBUILDING PIAWARE PACKAGE USING DEBIAN VER" ${OS_VERSION} "\e[39m"
 
 echo -e "\e[32mInstalling piaware dependencies \e[39m"
 
 #Depends:
-sudo apt install -y net-tools
-sudo apt install -y iproute2
-sudo apt install -y tclx8.4
-sudo apt install -y tcl8.6
-sudo apt install -y tcllib
-sudo apt install -y itcl3
+apt install -y net-tools
+apt install -y iproute2
+apt install -y tclx8.4
+apt install -y tcl8.6
+apt install -y tcllib
+apt install -y itcl3
 
 cd ${INSTALL_DIRECTORY}
 
 if [[ -d piaware_builder ]];
 then
 echo -e "\e[32mRenaming existing piaware_builder folder by adding prefix \"old\" \e[39m"
-sudo mv piaware_builder piaware_builder-old-$RANDOM
+mv piaware_builder piaware_builder-old-$RANDOM
 fi
 
 echo -e "\e[32mCloning piaware source code and building package \e[39m"
@@ -132,21 +135,18 @@ cd ${INSTALL_DIRECTORY}/piaware_builder
 git fetch --all
 git reset --hard origin/master
 echo -e "\e[32mBuilding the piaware package \e[39m"
-sudo ./sensible-build.sh ${OS_VERSION}
+./sensible-build.sh ${OS_VERSION}
 cd ${INSTALL_DIRECTORY}/piaware_builder/package-${OS_VERSION}
 
-##sudo sed -i 's/python3-dev(>=3.9)/python3-dev/' debian/control
-##sudo sed -i 's/tcl-tls (>= 1.7.22-2)/tcl-tls/' debian/control
-
-sudo dpkg-buildpackage -b --no-sign 
+dpkg-buildpackage -b --no-sign 
 PIAWARE_VER=$(grep "Version:" debian/piaware/DEBIAN/control | sed 's/^Version: //')
 
 echo -e "\e[32mInstalling piaware package\e[39m"
 cd ../
-sudo dpkg -i piaware_${PIAWARE_VER}_*.deb
+dpkg -i piaware_${PIAWARE_VER}_*.deb
 
-sudo systemctl enable piaware
-sudo systemctl restart piaware
+systemctl enable piaware
+systemctl restart piaware
 
 echo ""
 echo -e "\e[32mPIAWARE INSTALLATION COMPLETED \e[39m"
