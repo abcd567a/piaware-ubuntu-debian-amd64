60523d6ba8d684043559f7fe901075ce3bb1d98b
abcd567a <abcd567@hotmail.com>
2024-08-14 04:41:05 -0400

    Update install-dump1090-fa.sh

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -12,52 +12,53 @@ apt install -y lsb-release
 OS_ID=`lsb_release -si`
 OS_RELEASE=`lsb_release -sr`
 OS_VERSION=`lsb_release -sc`
+OS_EQV_VERSION=""
 
 echo -e "\e[35mDETECTED OS VERSION" ${OS_ID} ${OS_RELEASE} ${OS_VERSION}  "\e[39m"
 
 # DEBIAN
 if [[ ${OS_VERSION} == stretch ]]; then
-  OS_VERSION=stretch
+  OS_EQV_VERSION=stretch
 elif [[ ${OS_VERSION} == buster ]]; then
-  OS_VERSION=buster
+  OS_EQV_VERSION=buster
 elif [[ ${OS_VERSION} == bullseye ]]; then
-  OS_VERSION=bullseye
+  OS_EQV_VERSION=bullseye
 elif [[ ${OS_VERSION} == bookworm ]]; then
-  OS_VERSION=bookworm
+  OS_EQV_VERSION=bookworm
 elif [[ ${OS_VERSION} == trixie ]]; then
-  OS_VERSION=bookworm
+  OS_EQV_VERSION=bookworm
   
 ## UBUNTU
 elif [[ ${OS_VERSION} == bionic ]]; then
-  OS_VERSION=stretch
+  OS_EQV_VERSION=stretch
 elif [[ ${OS_VERSION} == focal ]]; then
-  OS_VERSION=buster
+  OS_EQV_VERSION=buster
 elif [[ ${OS_VERSION} == jammy ]]; then
-  OS_VERSION=bullseye
+  OS_EQV_VERSION=bullseye
 elif [[ ${OS_VERSION} == kinetic ]]; then
-  OS_VERSION=bullseye
+  OS_EQV_VERSION=bullseye
 elif [[ ${OS_VERSION} == lunar || ${OS_VERSION} == mantic || ${OS_VERSION} == noble ]]; then
-  OS_VERSION=bookworm
+  OS_EQV_VERSION=bookworm
   
 ## LINUX MINT
 elif [[ ${OS_VERSION} == tara || ${OS_VERSION} == tessa || ${OS_VERSION} == tina || ${OS_VERSION} == tricia ]]; then
-  OS_VERSION=stretch
+  OS_EQV_VERSION=stretch
 elif [[ ${OS_VERSION} == una || ${OS_VERSION} == uma || ${OS_VERSION} == ulyana || ${OS_VERSION} == ulyssa ]]; then
-  OS_VERSION=buster
+  OS_EQV_VERSION=buster
 elif [[ ${OS_VERSION} == vanessa || ${OS_VERSION} == vera || ${OS_VERSION} == victoria || ${OS_VERSION} == virginia ]]; then
-  OS_VERSION=bullseye
+  OS_EQV_VERSION=bullseye
 elif [[ ${OS_VERSION} == faye ]]; then
-  OS_VERSION=bookworm
+  OS_EQV_VERSION=bookworm
 
 ## KALI LINUX
 elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2021 ]]; then
-  OS_VERSION=buster
+  OS_EQV_VERSION=buster
 elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2022 ]]; then
-  OS_VERSION=bullseye
+  OS_EQV_VERSION=bullseye
 elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2023 ]]; then
-  OS_VERSION=bookworm
+  OS_EQV_VERSION=bookworm
 elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2024 ]]; then
-  OS_VERSION=bookworm
+  OS_EQV_VERSION=bookworm
   
 ## ANY OTHER
 else
@@ -65,7 +66,7 @@ else
    exit
 fi
 
-echo -e "\e[36mBUILDING PACKAGE USING DEBIAN VER" ${OS_VERSION} "\e[39m"
+echo -e "\e[36mBUILDING PACKAGE USING DEBIAN VER" ${OS_EQV_VERSION} "\e[39m"
 
 echo -e "\e[32mInstalling Build tools and Build dependencies\e[39m"
 
@@ -108,15 +109,17 @@ mv dump1090 dump1090-old-$RANDOM
 fi
 
 echo -e "\e[32mCloning dump1090-fa source code\e[39m"
-git clone https://github.com/flightaware/dump1090
+if [[ ${OS_VERSION} == trixie ]]; then
+  git clone -b dev https://github.com/flightaware/dump1090
+else
+  git clone https://github.com/flightaware/dump1090
+fi
 
 cd ${INSTALL_DIRECTORY}/dump1090
-git fetch --all
-git reset --hard origin/master
 
 echo -e "\e[32mBuilding dump1090-fa package\e[39m"
-./prepare-build.sh ${OS_VERSION}
-cd ${INSTALL_DIRECTORY}/dump1090/package-${OS_VERSION}
+./prepare-build.sh ${OS_EQV_VERSION}
+cd ${INSTALL_DIRECTORY}/dump1090/package-${OS_EQV_VERSION}
 
 dpkg-buildpackage -b --no-sign
 DUMP_VER=$(grep "Version:" debian/dump1090-fa/DEBIAN/control | sed 's/^Version: //')
