b5d9dcce37e14b84ac9deea7692920fec8d437f8
abcd567a <abcd567@hotmail.com>
2025-01-25 11:38:18 -0500

    Update install-dump1090-fa.sh

--- install-dump1090-fa.sh
+++ install-dump1090-fa.sh
@@ -49,7 +49,7 @@ elif [[ ${OS_VERSION} == una || ${OS_VERSION} == uma || ${OS_VERSION} == ulyana 
   OS_EQV_VERSION=buster
 elif [[ ${OS_VERSION} == vanessa || ${OS_VERSION} == vera || ${OS_VERSION} == victoria || ${OS_VERSION} == virginia ]]; then
   OS_EQV_VERSION=bullseye
-elif [[ ${OS_VERSION} == faye || ${OS_VERSION} == wilma ]]; then
+elif [[ ${OS_VERSION} == faye || ${OS_VERSION} == wilma || ${OS_VERSION} == xia ]]; then
   OS_EQV_VERSION=bookworm
 
 ## KALI LINUX
