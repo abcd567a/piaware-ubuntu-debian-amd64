eb92017dd00c7d8a9408a84b7e2a2c7f0531fcc0
abcd567a <28452511+abcd567a@users.noreply.github.com>
2026-08-17 19:46:43 -0400

    Add support for LMDE 7 'gigi'

--- install-piaware-web.sh
+++ install-piaware-web.sh
@@ -54,6 +54,8 @@ elif [[ ${OS_VERSION} == vanessa || ${OS_VERSION} == vera || ${OS_VERSION} == vi
   OS_VERSION=bullseye
 elif [[ ${OS_VERSION} == faye || ${OS_VERSION} == wilma || ${OS_VERSION} == xia || ${OS_VERSION} == zara ]]; then
   OS_VERSION=bookworm
+elif [[ ${OS_VERSION} == gigi ]]; then
+  OS_VERSION=trixie
 
 ## KALI LINUX
 elif [[ ${OS_ID} == Kali && ${OS_RELEASE%.*} == 2021 ]]; then
